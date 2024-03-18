0520e2a91c63b606e5e57b2df8ad01d09a54497d
Tylam.dev <tylam.dev@gmail.com>
2024-03-18 11:34:47 -0500

    deploy netlify

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBWEN5OoU5k72zSW8_ExyjT3DuaJZJP6lI",
-//   authDomain: "appvisitas-58c0f.firebaseapp.com",
-//   projectId: "appvisitas-58c0f",
-//   storageBucket: "appvisitas-58c0f.appspot.com",
-//   messagingSenderId: "395235742313",
-//   appId: "1:395235742313:web:d178e20ec6892e970e08f8",
-//   measurementId: "G-QJCEXEH9MH"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBWEN5OoU5k72zSW8_ExyjT3DuaJZJP6lI",
+  authDomain: "appvisitas-58c0f.firebaseapp.com",
+  projectId: "appvisitas-58c0f",
+  storageBucket: "appvisitas-58c0f.appspot.com",
+  messagingSenderId: "395235742313",
+  appId: "1:395235742313:web:d178e20ec6892e970e08f8",
+  measurementId: "G-QJCEXEH9MH"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
